bb28b8e4857f6aafdcec025b0f7352ed31cd15ab
Astrid-OC <astridtournant@gmail.com>
2024-10-07 11:07:04 +0200

    ✨ ajout du login & register global

--- migrations/Version20241004140307.php
+++ migrations/Version20241004140307.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241003133544 extends AbstractMigration
+final class Version20241004140307 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,25 @@ final class Version20241003133544 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, produits_commande JSON NOT NULL, date DATETIME NOT NULL, etat VARCHAR(255) NOT NULL, hour_recup VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE commande_historique (commande_id INT NOT NULL, historique_id INT NOT NULL, INDEX IDX_757DF90A82EA2E54 (commande_id), INDEX IDX_757DF90A6128735E (historique_id), PRIMARY KEY(commande_id, historique_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE commande_user (commande_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_E6FFD7AA82EA2E54 (commande_id), INDEX IDX_E6FFD7AAA76ED395 (user_id), PRIMARY KEY(commande_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, note INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comment_user (comment_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_ABA574A5F8697D13 (comment_id), INDEX IDX_ABA574A5A76ED395 (user_id), PRIMARY KEY(comment_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE etat (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE historique (id INT AUTO_INCREMENT NOT NULL, user_histo_id INT DEFAULT NULL, date DATETIME NOT NULL, INDEX IDX_EDBFD5ECE32E4B46 (user_histo_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE marche (id INT AUTO_INCREMENT NOT NULL, marche_name VARCHAR(255) NOT NULL, place VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE marche (id INT AUTO_INCREMENT NOT NULL, marche_name VARCHAR(255) NOT NULL, place VARCHAR(255) NOT NULL, image_file_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE marche_user (marche_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_B82784B29E494911 (marche_id), INDEX IDX_B82784B2A76ED395 (user_id), PRIMARY KEY(marche_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, user_product_id INT DEFAULT NULL, preoduct_name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, prix DOUBLE PRECISION NOT NULL, stock INT NOT NULL, disponibility TINYINT(1) NOT NULL, INDEX IDX_29A5EC27E2E3A0B6 (user_product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, user_product_id INT DEFAULT NULL, preoduct_name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, prix DOUBLE PRECISION NOT NULL, stock INT NOT NULL, disponibility TINYINT(1) NOT NULL, image_file_name VARCHAR(255) NOT NULL, INDEX IDX_29A5EC27E2E3A0B6 (user_product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE produit_commande (produit_id INT NOT NULL, commande_id INT NOT NULL, INDEX IDX_47F5946EF347EFB (produit_id), INDEX IDX_47F5946E82EA2E54 (commande_id), PRIMARY KEY(produit_id, commande_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, user_name VARCHAR(255) NOT NULL, tel VARCHAR(255) NOT NULL, name_business VARCHAR(255) DEFAULT NULL, stats JSON DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, user_name VARCHAR(255) NOT NULL, tel VARCHAR(255) NOT NULL, name_business VARCHAR(255) DEFAULT NULL, stats JSON DEFAULT NULL, image_file_name VARCHAR(255) NOT NULL, description_commerce LONGTEXT DEFAULT NULL, place_marche VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE commande_historique ADD CONSTRAINT FK_757DF90A82EA2E54 FOREIGN KEY (commande_id) REFERENCES commande (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE commande_historique ADD CONSTRAINT FK_757DF90A6128735E FOREIGN KEY (historique_id) REFERENCES historique (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE commande_user ADD CONSTRAINT FK_E6FFD7AA82EA2E54 FOREIGN KEY (commande_id) REFERENCES commande (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE commande_user ADD CONSTRAINT FK_E6FFD7AAA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE comment_user ADD CONSTRAINT FK_ABA574A5F8697D13 FOREIGN KEY (comment_id) REFERENCES comment (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE comment_user ADD CONSTRAINT FK_ABA574A5A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE historique ADD CONSTRAINT FK_EDBFD5ECE32E4B46 FOREIGN KEY (user_histo_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE marche_user ADD CONSTRAINT FK_B82784B29E494911 FOREIGN KEY (marche_id) REFERENCES marche (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE marche_user ADD CONSTRAINT FK_B82784B2A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
@@ -48,15 +54,21 @@ final class Version20241003133544 extends AbstractMigration
         $this->addSql('ALTER TABLE commande_historique DROP FOREIGN KEY FK_757DF90A6128735E');
         $this->addSql('ALTER TABLE commande_user DROP FOREIGN KEY FK_E6FFD7AA82EA2E54');
         $this->addSql('ALTER TABLE commande_user DROP FOREIGN KEY FK_E6FFD7AAA76ED395');
+        $this->addSql('ALTER TABLE comment_user DROP FOREIGN KEY FK_ABA574A5F8697D13');
+        $this->addSql('ALTER TABLE comment_user DROP FOREIGN KEY FK_ABA574A5A76ED395');
         $this->addSql('ALTER TABLE historique DROP FOREIGN KEY FK_EDBFD5ECE32E4B46');
         $this->addSql('ALTER TABLE marche_user DROP FOREIGN KEY FK_B82784B29E494911');
         $this->addSql('ALTER TABLE marche_user DROP FOREIGN KEY FK_B82784B2A76ED395');
         $this->addSql('ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC27E2E3A0B6');
         $this->addSql('ALTER TABLE produit_commande DROP FOREIGN KEY FK_47F5946EF347EFB');
         $this->addSql('ALTER TABLE produit_commande DROP FOREIGN KEY FK_47F5946E82EA2E54');
+        $this->addSql('DROP TABLE categorie');
         $this->addSql('DROP TABLE commande');
         $this->addSql('DROP TABLE commande_historique');
         $this->addSql('DROP TABLE commande_user');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE comment_user');
+        $this->addSql('DROP TABLE etat');
         $this->addSql('DROP TABLE historique');
         $this->addSql('DROP TABLE marche');
         $this->addSql('DROP TABLE marche_user');
