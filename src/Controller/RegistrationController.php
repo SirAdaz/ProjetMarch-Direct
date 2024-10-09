cf5d346910ae6c16ec8b757822cc06292da3af5e
yaya <elyayusd@gmail.com>
2024-10-09 10:15:27 +0200

    ✨ Ajout et Configuration du Mailer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,37 +6,56 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
-    {
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
+    public function register(
+        Request $request, 
+        UserPasswordHasherInterface $passwordHasher, 
+        EntityManagerInterface $entityManager,
+        MailerInterface $mailer
+    ): Response {
+        $data = json_decode($request->getContent(), true);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $plainPassword */
-            $plainPassword = $form->get('plainPassword')->getData();
+        $email = $data['username'] ?? null;
+        $password = $data['password'] ?? null;
+        $username = $data['username'] ?? null;
+        $tel = $data['0666666666'] ?? null;
 
-            // encode the plain password
-            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('app_login');
+        if (!$email || !$password) {
+            return new JsonResponse(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form,
-        ]);
+        $user = new User();
+        $user->setEmail($email);
+        $hashedPassword = $passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+        $user->setUserName($username);
+        $user->setTel($tel);
+        $user->setDateDeCreation(new \DateTime());
+        $user->setImageFileName("images/profil.png");
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Envoi d'un email à l'utilisateur
+        $emailMessage = (new Email())
+            ->from('elyayusd@gmail.com') // Boîte d'envoi
+            ->to($user->getEmail()) // Envoi de l'email à l'utilisateur enregistré
+            ->subject('Bienvenue sur Marché Direct !')
+            ->text('Bonjour '.$username .', profitez de tous vos produits préférés.')
+            ->html('<p>Merci de vous être inscrit sur notre site !</p>');
+
+        $mailer->send($emailMessage);
+
+        return new JsonResponse(['message' => 'User registered successfully'], Response::HTTP_CREATED);
     }
 }
