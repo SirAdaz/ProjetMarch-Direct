0778265feab1b6a9caf2c7ef38dc3362c47e7213
elyayusd <133009649+elyayus@users.noreply.github.com>
2024-10-16 14:28:46 +0200

    ✨Création du Token personnalisé pour recup l'id user

---
+++ src/Security/CustomJWTEncoder.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Security;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class CustomJWTEncoder
+{
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $user = $event->getUser();
+
+        // Vérifiez que l'utilisateur est bien une instance de User
+        if (!$user instanceof User) {
+            return;
+        }
+
+        // Récupérez les données actuelles du token
+        $data = $event->getData();
+
+        // Ajoutez les informations supplémentaires au token
+        $data['id'] = $user->getId();
+        $data['username'] = $user->getUsername();
+        $data['roles'] = $user->getRoles();
+        $data['email'] = $user->getEmail();
+        $data['tel'] = $user->getTel();
+        $data['nameBusiness'] = $user->getNameBusiness();
+        $data['stats'] = $user->getStats();
+
+        // Mettez à jour les données du token avec les nouvelles informations
+        $event->setData($data);
+    }
+}
