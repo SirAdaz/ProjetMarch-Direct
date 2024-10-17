faf0935fb050bf0951ce0cc03a2363af9d322ac0
yaya <elyayusd@gmail.com>
2024-10-17 13:53:20 +0200

    Récupération id user

--- src/Security/CustomJWTEncoder.php
+++ src/Security/CustomJWTEncoder.php
@@ -18,18 +18,13 @@ class CustomJWTEncoder
         $data = $event->getData();
 
         // Ajoutez les informations supplémentaires au token
-        
 
         $data['id'] = $user->getId();
         $data['username'] = $user->getUsername();
         $data['roles'] = $user->getRoles();
         $data['email'] = $user->getEmail();
 
-        $data['tel'] = $user->getTel();
-        $data['nameBusiness'] = $user->getNameBusiness();
-        $data['stats'] = $user->getStats();
-
-        // Mettez à jour les données du token avec les nouvelles informations
+        // Mise à jour des données du token avec les nouvelles informations
         $event->setData($data);
       }  
     }
